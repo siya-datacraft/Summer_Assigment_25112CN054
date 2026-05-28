eb856ffbced4ed0406cd057eddcabedb4d0b7fa5
Siya Kanaujiya <kanaujiasiya@gmail.com>
2026-05-28 23:37:00 +0530

    Create natural number sum.c

---
+++ day 01/natural number sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter a number (N): ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++){
+        sum += i;
+    }
+    printf("The sum of the frist %d natural numbers is: %d\n", n, sum);
+    return 0;
+}
