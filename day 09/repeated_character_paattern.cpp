c7f4eeb7a99c14267ebffaeac2a4a3085f205f4a
Siya Kanaujiya <kanaujiasiya@gmail.com>
2026-06-09 00:07:29 +0530

    Add files via upload

---
+++ day 09/repeated_character_paattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows = 5;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << (char)('A' + i);
+        }
+        cout << endl;
+    }
+    return 0;
+}
