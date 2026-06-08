59b1b0fc3cfa554fd3558237b8bb8290c69f9379
Siya Kanaujiya <kanaujiasiya@gmail.com>
2026-06-09 00:06:29 +0530

    Add files via upload

---
+++ day 09/hollow_square_pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size = 5;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+            {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
