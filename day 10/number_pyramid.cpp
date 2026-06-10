3cc9a9a31c5bd3604705de738771c319bc1fc5a9
Siya Kanaujiya <kanaujiasiya@gmail.com>
2026-06-10 23:32:43 +0530

    Add files via upload

---
+++ day 10/number_pyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows = 5;
+    for(int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= i; j++) 
+        {
+            cout << j;
+        }
+        for (int j = i - 1; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
