3dc54458487d1f19578b5f1c504d48ec66eda5fb
Siya Kanaujiya <kanaujiasiya@gmail.com>
2026-06-19 23:23:42 +0530

    Add files via upload

---
+++ day 16/max_frequency.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    int arr[100];
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+    int maxCount = 0;
+    int maxElement = arr[0];
+    for (int i = 0; i < n; i++) 
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++) 
+        {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if (count > maxCount) 
+        {
+            maxCount = count;
+            maxElement = arr[i];
+        }
+    }
+    cout << "Element with maximum frequency: " << maxElement << endl;
+    cout << "Frequency: " << maxCount;
+    return 0;
+}
