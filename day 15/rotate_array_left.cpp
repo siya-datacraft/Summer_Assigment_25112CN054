64d4e838a017150445368ebd8780d383edab8e9e
Siya Kanaujiya <kanaujiasiya@gmail.com>
2026-06-19 22:57:24 +0530

    Add files via upload

---
+++ day 15/rotate_array_left.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++)
+     {
+        cin >> arr[i];
+    }
+    int first = arr[0];
+    for(int i = 0; i < n - 1; i++) 
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[n - 1] = first;
+    cout << "Array after left rotation:\n";
+    for(int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
