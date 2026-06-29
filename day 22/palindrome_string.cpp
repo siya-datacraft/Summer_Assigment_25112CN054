454a3031f860ea632d54f8296aa2d21bb1c74016
Siya Kanaujiya <kanaujiasiya@gmail.com>
2026-06-30 00:18:47 +0530

    Update palindrome_string.cpp

--- day 22/palindrome_string.cpp
+++ day 22/palindrome_string.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string input, result = "";
-
+int main() 
+{
+    string str;
+    bool isPalindrome = true;
     cout << "Enter a string: ";
-    getline(cin, input);
-
-    // build new string without spaces
-    for (char ch : input) {
-        if (ch != ' ') {
-            result += ch;
+    getline(cin, str);
+    int left = 0;
+    int right = str.length() - 1;
+    while (left < right) 
+    {
+        if (str[left] != str[right]) 
+        {
+            isPalindrome = false;
+            break;
         }
+        left++;
+        right--;
     }
-
-    cout << "After removing spaces: " << result;
-
+    if (isPalindrome) 
+    {
+        cout << "It is a Palindrome";
+    } else 
+    {
+        cout << "Not a Palindrome";
+    }
     return 0;
 }
